5440588fb97af0081b5c62a51232b0fcf66226a6
Ezra Earl Villanueva <ezraearlvillanueva@gmail.com>
2025-09-30 00:30:46 +0800

    Add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,8 +9,9 @@ import "easymde/dist/easymde.min.css";
 import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
+import { set, z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -19,7 +20,8 @@ const NewIssuePage = () => {
   const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema)
   });
-  const [error, setError] = useState<string | null>(null);
+  const [ error, setError ] = useState<string | null>(null);
+  const [ isSubmitting, setSubmitting ] = useState(false);
 
   return (
     <div className='max-w-xl '>
@@ -32,9 +34,11 @@ const NewIssuePage = () => {
         className='space-y-3'
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post('/api/issues', data);
             router.push('/issues');
           } catch (error) {
+            setSubmitting(false);
             setError('Unexpected error occured.');
           }
         })}
@@ -50,7 +54,7 @@ const NewIssuePage = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
       </form>
     </div>
   )
